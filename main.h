217bab7e36bbebee80c9bcfb5913146e1e551541
comidaparallevar <lyanosorio98@gmail.com>
2023-11-25 15:22:36 -0400

    add files to copy prototypes

--- main.h
+++ main.h
@@ -1,4 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+int _printf(const char *, ...);
+int (*get_f(char specifier))(char *, va_list)
+
+int f_c(int c);
+int f_s(va_list arg);
+int f_mod(va_list arg);
+
+/**
+ * struct get_functions - struct to get function of spec
+ * @s: specifier
+ * @f: function to use
+ */
+
+typedef struct get_functions
+{
+  char s;
+  int (*f)(va_list);
+} get_f;
 
 #endif
